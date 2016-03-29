db1d67fb014a82cb1861df61dda4ab75add52388
Damien Mehala <damien.mehala@me.com>
2016-03-29 22:50:01 +0200

    Pretty Printing!

--- src/acgrep_source.c
+++ src/acgrep_source.c
@@ -9,6 +9,8 @@
 
 // CONST VARIABLE
 const int buffer_size = 1024;
+const char *ansi_green_color = "\x1b[1;32m";
+const char *ansi_reset_color = "\x1b[0m";
 
 inline void print_usage(const char* appName) {
 	printf("Usage: %s fichier repertoire\n", appName);
@@ -22,8 +24,11 @@ int main(int argc, char** argv)
 	}
 
 	const char* fichier = argv[1];
-	const char* repertoire = argv[2];
 
+	// Normalise le repertoire
+	strcat(argv[2], "/");
+	const char* repertoire = argv[2];
+	
 	FILE *txt = fopen(fichier, "r");
 	if(txt == NULL) {
 		fprintf(stderr, "%s : Fichier \"%s\" introuvable\n", __FILE__, fichier);
@@ -42,6 +47,8 @@ int main(int argc, char** argv)
 			buffer[i] = '\0';
 
 			list_words[j] = (char*)malloc(sizeof(char) * i+1);
+			// Normalise l'entrée
+			//list_words[j] = normalize_string(buffer);
 			memcpy(list_words[j++], buffer, i+1);
 
 			i = 0;
@@ -62,36 +69,70 @@ int main(int argc, char** argv)
 	struct dirent* cur_file = NULL;
 
 	if((cur_dir = opendir(repertoire)) == NULL) {
-		fprintf(stderr, "%s: Impossible d'accer au repertoire \"%s\"", __FILE__, repertoire);
+		fprintf(stderr, "%s: Impossible d'acceder au repertoire \"%s\"\n", __FILE__, repertoire);
 		exit(1);
 	}
 
 	// Parcours du répertoire
+	int occ = 0, tmpocc = 0, sz_correspondance = 0, line = 1;
+	char path[buffer_size];
+	memset(buffer, 0, buffer_size);
 	while((cur_file = readdir(cur_dir)) != NULL)
 	{
 		if(cur_file->d_type == DT_REG)
 		{
-			txt = fopen(cur_file->d_name, "r");
+			// Construit le chemin du fichier
+			memset(path, 0, buffer_size);
+			strcat(path, repertoire);
+			strcat(path, cur_file->d_name);
+
+			txt = fopen(path, "r");
 			if(txt == NULL) {
 				fprintf(stderr, "%s : Fichier \"%s\" introuvable\n", __FILE__, cur_file->d_name);
 				exit(1);
 			}
 
-			printf("=================\nFichier: %s\n=================\n", cur_file->d_name);
+			i = 0;
+			occ = 0, tmpocc = 0, sz_correspondance = 0, line = 1;
 			while((c = tolower(fgetc(txt))) != EOF)
 			{
+				buffer[i%buffer_size] = c;
+				i = i + 1;
+
 				if(!is_valid_caractere((int)c))
 					continue;
 
+				if(c == '\n')
+				{
+					if(occ > tmpocc) {
+						if(tmpocc == 0) {printf("=================\nFichier: %s\n=================\n", cur_file->d_name);}
+						buffer[i-1] = '\0';
+						printf("\x1b[1;36ml.%d\x1b[0m -  %s\n", line, buffer);
+						tmpocc++;
+					}
+
+					i = 0;
+					line = line + 1;
+					memset(buffer, 0, buffer_size);
+				}
+
 				read_letter(Dx, c);
-				if(is_final(Dx)) {
-					printf("Occurence trouvée: %s\n", Dx->correspondance[Dx->cursor]);
+				if(is_final(Dx))
+				{
+					// Pretty Printing !
+					sz_correspondance = strlen(Dx->correspondance[Dx->cursor]);
+					buffer[i-sz_correspondance] = '\0';
+					strcat(buffer, ansi_green_color);
+					strcat(buffer, Dx->correspondance[Dx->cursor]);
+					strcat(buffer, ansi_reset_color);
+					i += 11;
+					occ = occ + 1;
 				}
 			}
 
 			fclose(txt);
 		}
-		printf("\n");
+		//printf("\n");
 	}
 	
 
