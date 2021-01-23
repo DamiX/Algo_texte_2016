77421f7b154f32946b87a5c6a9b44d55eff51cc0
Damien Mehala <damien.mehala@gmail.com>
2021-01-23 14:59:07 +0100

    Remove c++11 sources

--- c++11/src/sgrep.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "shellColors.h"
-
-int main(int argc, char** argv) {
-    std::cout << shell::Red << "Coucou, le monde!" << shell::Default << std::endl;
-    std::cout << shell::Style(shell::green, shell::underline) << "Hello, world!" << shell::Default << std::endl;
-    return 0;
-}
